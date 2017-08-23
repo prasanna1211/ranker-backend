c5fb3f904d32f72edf647cd0af26226bf4224915
prasanna1211 <prasanna.krystal@gmail.com>
2017-08-23 14:08:42 +0530

    tested base route

--- src/api/index.js
+++ src/api/index.js
@@ -5,12 +5,11 @@ import facets from './facets';
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
-
-	// perhaps expose some API metadata at the root
+	// modified root API
 	api.get('/', (req, res) => {
-		res.json({ version });
+		res.json({
+			success: false,
+		});
 	});
 
 	return api;
