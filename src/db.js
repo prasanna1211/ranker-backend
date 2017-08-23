b11a153e849e14bea5d0d8c4b60446de01af18a8
prasanna1211 <prasanna.krystal@gmail.com>
2017-08-23 14:34:45 +0530

    Added sql and added connections

--- src/db.js
+++ src/db.js
@@ -1,4 +1,16 @@
+import mysql from 'mysql';
+import config from './config.json';
+
 export default callback => {
 	// connect to a database if needed, then pass it to `callback`:
-	callback();
+	const con = mysql.createConnection({
+		host: config.dbHost,
+		user: config.dbUsername,
+		password: config.dbPassword,
+	});
+
+	con.connect((err) => {
+		if (err) throw err;
+	})
+	callback(con);
 }
