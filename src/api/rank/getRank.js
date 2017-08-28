c9017cd2094200d706d71691598b5bb3ca08fdcf
prasanna1211 <prasanna.krystal@gmail.com>
2017-08-28 06:09:35 +0530

    fixed sorting + keyword addition

--- src/api/rank/getRank.js
+++ src/api/rank/getRank.js
@@ -4,15 +4,20 @@ export default (db) => (req, res) => {
       startDate,
       endDate,
       company,
+      keyword,
     },
   } = req;
 
   const query = `SELECT r.logDate, r.rank
-  FROM ranks r, companies c
+  FROM ranks r, companies c, keywords k
   WHERE logDate between '${startDate}' and '${endDate}'
   AND c.name = '${company}'
+  AND k.keyword = '${keyword}'
   ORDER BY r.logDate DESC
   `;
+
+  console.log(query);
+
   db.query(query, (error, result) => {
     if (error) throw error;
     res.json(result);
