db7e38a6900645b2b9761c999d2d1f80bca74902
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-01 17:29:11 +0530

    added automation fix

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -19,36 +19,41 @@ async function run() {
   await page.waitForNavigation();
 
   const pageOneText = await page.plainText();
-  let result = getUrls(pageOneText);
+  let result = [...getUrls(pageOneText)];
 
-  await page.waitFor(4000);
+  console.log('til page 1 ....... ', result);
 
   const pageTwoNavigateButton = await page.$('a[aria-label="Page 2"]');
-  pageTwoNavigateButton.click();
+  await pageTwoNavigateButton.click();
+  await page.waitForNavigation();
 
-  await page.waitFor(4000);
-  const pageThreeText = await page.plainText();
-  result.add(getUrls(pageThreeText));
+  const pageTwoText = await page.plainText();
+  result.push(...getUrls(pageTwoText));
+
   const pageThreeNavigateButton = await page.$('a[aria-label="Page 3"]');
-  pageThreeNavigateButton.click();
+  await pageThreeNavigateButton.click();
+  await page.waitForNavigation();
 
-  await page.waitFor(4000);
-  const pageFourText = await page.plainText();
-  result.add(getUrls(pageFourText));
+  const pageThreeText = await page.plainText();
+  result.push(...getUrls(pageThreeText));
+
   const pageFourNavigateButton = await page.$('a[aria-label="Page 4"]');
-  pageFourNavigateButton.click();
+  await pageFourNavigateButton.click();
+  await page.waitForNavigation();
 
-  await page.waitFor(4000);
-  const pageFiveText = await page.plainText();
-  result.add(getUrls(pageFiveText));
+  const pageFourText = await page.plainText();
+  result.push(...getUrls(pageFourText));
+
   const pageFiveNavigateButton = await page.$('a[aria-label="Page 5"]');
-  pageFiveNavigateButton.click();
+  await pageFiveNavigateButton.click();
 
   await page.waitForNavigation();
-  console.log(result);
-  await page.waitFor(40000);
 
-  await page.screenshot({path: 'screenshots/github.png'});
+  const pageFiveText = await page.plainText();
+  result.push(...getUrls(pageFiveText));
+
+  console.log(result);
+
 
   browser.close();
 }
