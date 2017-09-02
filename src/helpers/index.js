10f9fc8a86bb87a3c012df5e62e03dd413405a5a
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-02 13:31:04 +0530

    modified scrapping function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,61 +1,73 @@
 const puppeteer = require('puppeteer');
 const getUrls = require('get-urls');
 
-async function run() {
+const times = require('lodash/times');
+const concat = require('lodash/concat');
+const isNull = require('lodash/isNull');
+
+const getRandomSeconds = require('./getRandomSecondsBetweenRange.js');
+
+const appConstants = require('./appConstants.js');
+
+async function scrapeGoogleResult(keyword, domain, numberOfPages) {
+
+  const {
+    pageWaitDurationRangeStart: start,
+    pageWaitDurationRangeEnd: end,
+  } = appConstants;
+
   const browser = await puppeteer.launch({
     headless: false,
   });
   const page = await browser.newPage();
 
-  await page.goto('https://www.google.com');
+  await page.goto(domain);
 
   page.focus('input[title="Search"]');
 
-  await page.type('React JS development company', { delay: 100 });
+  await page.type(keyword, { delay: getRandomSeconds(100, 200) });
 
   const submit = await page.$('input[value="Google Search"]');
   await submit.click();
 
   await page.waitForNavigation();
 
+  // const result
   const pageOneText = await page.plainText();
-  let result = [...getUrls(pageOneText)];
+  if (isNull(pageOneText)) {
+    return {
+      error: true,
+      errorMessage: 'Failed to fetch page 1'
+    };
+  }
+  let result = Array.from(getUrls(pageOneText));
 
-  console.log('til page 1 ....... ', result);
+  let index = 1;
+  for (var i = index + 1; i <= numberOfPages ; i++) {
+    const selector = `a[aria-label="Page ${i}"]`;
+    const nextPageNavigateButton = await page.$(selector);
 
-  const pageTwoNavigateButton = await page.$('a[aria-label="Page 2"]');
-  await pageTwoNavigateButton.click();
-  await page.waitForNavigation();
+    await page.waitFor(getRandomSeconds(start, end));
+    await nextPageNavigateButton.click();
+    await page.waitForNavigation();
 
-  const pageTwoText = await page.plainText();
-  result.push(...getUrls(pageTwoText));
-
-  const pageThreeNavigateButton = await page.$('a[aria-label="Page 3"]');
-  await pageThreeNavigateButton.click();
-  await page.waitForNavigation();
-
-  const pageThreeText = await page.plainText();
-  result.push(...getUrls(pageThreeText));
-
-  const pageFourNavigateButton = await page.$('a[aria-label="Page 4"]');
-  await pageFourNavigateButton.click();
-  await page.waitForNavigation();
-
-  const pageFourText = await page.plainText();
-  result.push(...getUrls(pageFourText));
-
-  const pageFiveNavigateButton = await page.$('a[aria-label="Page 5"]');
-  await pageFiveNavigateButton.click();
-
-  await page.waitForNavigation();
-
-  const pageFiveText = await page.plainText();
-  result.push(...getUrls(pageFiveText));
-
-  console.log(result);
-
+    const currentPageText = await page.plainText();
+    if (isNull(currentPageText)) {
+      return {
+        error: true,
+        errorMessage: `Failed to fetch page ${i}`
+      };
+    }
+    result = concat(result, Array.from(getUrls(currentPageText)));
+  }
 
   browser.close();
+  return {
+    success: true,
+    result,
+  };
 }
 
-run();
+scrapeGoogleResult('React js development company', 'https://www.google.com', 5).then((result) => {
+  console.log(result);
+});
