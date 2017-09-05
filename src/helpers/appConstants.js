7dc5b1df377a1a8a194612d636d6ac08b69d0fd1
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-05 19:33:37 +0530

    Changed constants

--- src/helpers/appConstants.js
+++ src/helpers/appConstants.js
@@ -1,8 +1,8 @@
 const getRandomSecondsBetweenRange = require('./getRandomSecondsBetweenRange.js');
 
-const PAGE_WAIT_DURATION_RANGE_START = getRandomSecondsBetweenRange(2000, 2500);
-const PAGE_WAIT_DURATION_RANGE_END = getRandomSecondsBetweenRange(3500, 5000);
-const DELAY_BETWEEN_KEYWORDS = getRandomSecondsBetweenRange(4000-9000);
+const PAGE_WAIT_DURATION_RANGE_START = getRandomSecondsBetweenRange(3000, 8000);
+const PAGE_WAIT_DURATION_RANGE_END = getRandomSecondsBetweenRange(9000, 20000);
+const DELAY_BETWEEN_KEYWORDS = getRandomSecondsBetweenRange(5000, 30000);
 
 module.exports = {
   pageWaitDurationRangeStart: PAGE_WAIT_DURATION_RANGE_START,
