70487d52393cc56d8bdaf8331f311a0de79933aa
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-05 19:33:26 +0530

    Added scrap and write to db

--- src/api/sync/scrapAndWriteToDb.js
+++ src/api/sync/scrapAndWriteToDb.js
@@ -10,13 +10,15 @@ const appConstants = require('../../helpers/appConstants.js');
 const moment = require('moment');
 
 export default (db) => (req, res) => {
+  const start = new Date().getTime();
   res.json({
     started: true,
   });
-
+  let hasErrorOccured = false;
   // query DB for all keywords
   const keywordQuery = 'SELECT id, keyword from keywords';
   const companyQuery = 'SELECT id, name, url from companies';
+  const domainQuery = 'SELECT'
   const rankQuery = 'INSERT INTO ranks'
 
   let keywords;
@@ -28,45 +30,60 @@ export default (db) => (req, res) => {
     let companyPromise = db.query(companyQuery, (error, result) => {
       if (error) throw error;
       companies = result;
+
       const companyUrl = map(companies, ({ url }) => url);
-      const allScrappedLinkPromise = Promise.mapSeries(keywords, (word) => {
-        return Promise
-        .delay(appConstants.delayBetweenKeywords)
-        .then(() => {
-          return scrap.scrapeGoogleResult(word.keyword, 'https://www.google.co.in', 5)
-          .then((scrap) => {
-            if (scrap.success) {
-              let rankMapping = companyUrl.map(url => {
-                return {
-                  url,
-                  rank: findIndex(scrap.result, resultUrl => url === resultUrl),
-                  keywordId: word.id,
-                }
-              });
-              const rankData = map(rankMapping, (rank, index) => {
-                return merge(rank, companies[index]);
-              });
+        // recreate all domains here
+        Promise.mapSeries(keywords, (word) => {
+          return Promise
+          .delay(appConstants.delayBetweenKeywords)
+          .then(() => {
+            return scrap.scrapeGoogleResult(word.keyword, 'https://www.google.co.in', 5)
+            .then((scrap) => {
+              if (scrap.success) {
+                let rankMapping = companyUrl.map(url => {
+                  return {
+                    url,
+                    rank: findIndex(scrap.result, resultUrl => url === resultUrl),
+                    keywordId: word.id,
+                  }
+                });
+                const rankData = map(rankMapping, (rank, index) => {
+                  return merge(rank, companies[index]);
+                });
 
-              const currentDate = moment().startOf('day').format('YYYY-MM-DD');
-              const keywordQuery = 'KEYWORD QUERY';
+                const currentDate = moment().startOf('day').format('YYYY-MM-DD');
+                const keywordQuery = 'KEYWORD QUERY';
 
-              forEach(rankData, ({ url, rank, name, id, keywordId }) => {
-                let dbQuery = `INSERT INTO ranks (rank, logDate, companyId, keywordId, searchEngineId)
-                  VALUES (${rank !== -1 ? rank+1 : rank}, "${currentDate}", ${id}, ${keywordId}, 1);
-                `;
+                forEach(rankData, ({ url, rank, name, id, keywordId }) => {
+                  let dbQuery = `INSERT INTO ranks (rank, logDate, companyId, keywordId, searchEngineId)
+                    VALUES (${rank !== -1 ? rank+1 : rank}, "${currentDate}", ${id}, ${keywordId}, 1);
+                  `;
+                  db.query(dbQuery, (error, result) => {
+                    if (error) throw error;
+                  });
+                });
+              } else if (scrap.error) {
+                hasErrorOccured = true;
+                let dbQuery = `INSERT INTO errors (error) VALUE ${error.errorMessage}`
                 db.query(dbQuery, (error, result) => {
                   if (error) throw error;
-                  console.log(' written to db ');
+                  console.log(error);
                 });
-              });
-            }
-          })
+              }
+            })
+          });
+        }).then(() => {
+          const status = hasErrorOccured ? 'ERROR occured' : 'No Error Occured';
+          let dbQuery = `INSERT INTO statuses (status) VALUE ("${status}")`;
+          db.query(dbQuery, (error, result) => {
+            if (error) throw error;
+            const end = new Date().getTime();
+            console.log('finished ', (end - start) / 1000);
+          });
         });
-      });
     });
+
   });
-
-
 
 
   // For all scrap data
