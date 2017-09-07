04c7feddb38dcf3fc001e4f352afab30d7e79975
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-07 08:44:57 +0530

    separated scrapping logic

--- src/api/sync/scrapAndWriteToDb.js
+++ src/api/sync/scrapAndWriteToDb.js
@@ -35,8 +35,8 @@ export default (db) => (req, res) => {
         db.query(companyQuery, (error, result) => {
           if (error) throw error;
           companies = result;
-          const companyUrl = map(companies, ({ url }) => url);
-          console.log('searchEngines', searchEngines);
+
+          // console.log('searchEngines', searchEngines);
           // console.log('-------------->');
           // console.log('keywords', keywords);
           // console.log('-------------->');
@@ -45,65 +45,11 @@ export default (db) => (req, res) => {
           const randomNumber = map(keywords, () => ({ randomNumber: parseInt(Math.random()*10000) }) );
           keywords = merge(keywords, randomNumber);
           keywords = sortBy(keywords, keyword => keyword.randomNumber);
-          console.log(' keywords ', keywords);
-
-          Promise.mapSeries(searchEngines, ({ searchEngineId, searchEngineName, searchEngineUrl }) => {
-            // recreate all domains here
-            return Promise.mapSeries(keywords, (word) => {
-              return Promise
-              .delay(appConstants.delayBetweenKeywords)
-              .then(() => {
-                console.log(word.keyword, searchEngineUrl);
-                return scrap.scrapeGoogleResult(word.keyword, searchEngineUrl, 5)
-                .then((scrap) => {
-                  if (scrap.success) {
-                    let rankMapping = companyUrl.map(url => {
-                      return {
-                        url,
-                        rank: findIndex(scrap.result, resultUrl => url === resultUrl),
-                        keywordId: word.keywordId,
-                      }
-                    });
-                    const rankData = map(rankMapping, (rank, index) => {
-                      return merge(rank, companies[index]);
-                    });
-
-                    const currentDate = moment().startOf('day').format('YYYY-MM-DD');
-                    const keywordQuery = 'KEYWORD QUERY';
-
-                    forEach(rankData, ({ url, rank, name, companyId, keywordId }) => {
-                      let dbQuery = `INSERT INTO ranks (rank, logDate, companyId, keywordId, searchEngineId)
-                        VALUES (${rank !== -1 ? rank+1 : rank}, "${currentDate}", ${companyId}, ${keywordId}, ${searchEngineId});
-                      `;
-                      console.log(dbQuery);
-                      db.query(dbQuery, (error, result) => {
-                        if (error) throw error;
-                      });
-                    });
-                  } else if (scrap.error) {
-                    hasErrorOccured = true;
-                    let dbQuery = `INSERT INTO errors (error) VALUE ${error.errorMessage}`
-                    db.query(dbQuery, (error, result) => {
-                      if (error) throw error;
-                      console.log(error);
-                    });
-                  }
-                })
-              });
-            }).then(() => {
-              const status = hasErrorOccured ? 'ERROR occured' : 'No Error Occured';
-              let dbQuery = `INSERT INTO statuses (status) VALUE ("${status}")`;
-              db.query(dbQuery, (error, result) => {
-                if (error) throw error;
-                const end = new Date().getTime();
-                console.log('finished ', (end - start) / 1000);
-              });
-            });
-
+          scrapModule.scrapFunction(db, searchEngines, keywords, companies);
           });
       });
     });
-  });
+
   // For all scrap data
 
   // Once scrapped write in the db
