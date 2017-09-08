8edd3e1940175e0681eb887db55db621ed4e91af
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-08 09:52:26 +0530

    Added api for domains

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,16 @@ import rankRoutes from './rank/index.js';
 import companyRoutes from './companies/index.js';
 import keywordRoutes from './keywords/index.js';
 import syncRoutes from './sync/index.js';
+import domainRoutes from './domains/index.js';
 
 export default ({ config, db }) => {
 	let api = Router();
 
 	// rank related routes
 	api.route('/rank').get(rankRoutes.getRank(db));
+
+	// companies route
+	api.route('/domains').get(domainRoutes.getDomains(db));
 
 	// companies route
 	api.route('/companies').get(companyRoutes.getAllCompanies(db));
