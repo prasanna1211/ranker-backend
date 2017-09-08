15dcbeb5f223bc1efa7131a72451477d6688b2d1
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-08 08:22:51 +0530

    scrap function updated with new way

--- src/api/sync/scrapFunction.js
+++ src/api/sync/scrapFunction.js
@@ -27,43 +27,46 @@ const scrapFunction = (db, searchEngines, keywords, companies) => {
       console.log(' company url ', companyUrl);
       console.log('---------------------------------------------------------------->')
       return Promise
-      .delay(1000)
+      .delay(appConstants.delayBetweenKeywords)
       .then(() => {
 
-        // return scrap.scrapeGoogleResult(word.keyword, searchEngineUrl, 5)
-        // .then((scrap) => {
-        //   if (scrap.success) {
-        //     let rankMapping = companyUrl.map(url => {
-        //       return {
-        //         url,
-        //         rank: findIndex(scrap.result, resultUrl => url === resultUrl),
-        //         keywordId: word.keywordId,
-        //       }
-        //     });
-        //     const rankData = map(rankMapping, (rank, index) => {
-        //       return merge(rank, companies[index]);
-        //     });
-        //     const currentDate = moment().startOf('day').format('YYYY-MM-DD');
-        //     const keywordQuery = 'KEYWORD QUERY';
-        //
-        //     forEach(rankData, ({ url, rank, name, companyId, keywordId }) => {
-        //       let dbQuery = `INSERT INTO ranks (rank, logDate, companyId, keywordId, searchEngineId)
-        //         VALUES (${rank !== -1 ? rank+1 : rank}, "${currentDate}", ${companyId}, ${keywordId}, ${searchEngineId});
-        //       `;
-        //       console.log(dbQuery);
-        //       db.query(dbQuery, (error, result) => {
-        //         if (error) throw error;
-        //       });
-        //     });
-        //   } else if (scrap.error) {
-        //     hasErrorOccured = true;
-        //     let dbQuery = `INSERT INTO errors (error) VALUE ${error.errorMessage}`
-        //     db.query(dbQuery, (error, result) => {
-        //       if (error) throw error;
-        //       console.log(error);
-        //     });
-        //   }
-        // });
+        return scrap.scrapeGoogleResult(word.keyword, searchEngineUrl, 1)
+        .then((scrap) => {
+          if (scrap.success) {
+            console.log('scrap result', scrap.result);
+            console.log('company url', companyUrl);
+            let rankMapping = companyUrl.map(url => {
+              return {
+                url,
+                rank: findIndex(scrap.result, resultUrl => url === resultUrl),
+                keywordId: word.keywordId,
+              }
+            });
+            console.log('rankmapping ', rankMapping);
+            const rankData = map(rankMapping, (rank, index) => {
+              return merge(rank, companies[index]);
+            });
+            const currentDate = moment().startOf('day').format('YYYY-MM-DD');
+            const keywordQuery = 'KEYWORD QUERY';
+
+            forEach(rankData, ({ url, rank, name, companyId, keywordId }) => {
+              let dbQuery = `INSERT INTO ranks (rank, logDate, companyId, keywordId, searchEngineId)
+                VALUES (${rank !== -1 ? rank+1 : rank}, "${currentDate}", ${companyId}, ${keywordId}, ${searchEngineId});
+              `;
+              console.log(dbQuery);
+              db.query(dbQuery, (error, result) => {
+                if (error) throw error;
+              });
+            });
+          } else if (scrap.error) {
+            hasErrorOccured = true;
+            let dbQuery = `INSERT INTO errors (error) VALUE ${error.errorMessage}`
+            db.query(dbQuery, (error, result) => {
+              if (error) throw error;
+              console.log(error);
+            });
+          }
+        });
 
       });
     }).then(() => {
