105333259688067d1df8caa02f5c3f601378fc79
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-14 16:56:15 +0530

    updated sync to db

--- src/api/sync/scrapAndWriteToDb.js
+++ src/api/sync/scrapAndWriteToDb.js
@@ -12,19 +12,19 @@ const scrapModule = require('./scrapFunction.js');
 
 export default (db) => (req, res) => {
   const start = new Date().getTime();
-  console.log('started----');
+
   res.json({
     started: true,
   });
 
   // query DB for all keywords
-  const keywordQuery = 'select k.keyword, k.id as keywordId, d.domain, d.category, d.id as domainId from keywords k, domains d where k.domain_id = d.id';
-  const companyQuery = 'SELECT c.id as companyId, cn.name, c.url, d.domain from companies c, domains d, companynames cn where d.id = c.domainId AND c.companynameId = cn.id';
+  const keywordQuery = 'select k.keyword, k.id as keywordId, k.isImportant as isImportant, d.domain, d.id as domainId from keywords k, domains d where k.domainId = d.id';
+  const urlQuery = 'SELECT u.id as urlId, u.url, cn.name, d.domain from urls u, domains d, companynames cn where d.id = u.domainId AND u.companynameId = cn.id';
   const rankQuery = 'INSERT INTO ranks'
   const searchEngineQuery = 'SELECT id as searchEngineId, name as searchEngineName, value as searchEngineUrl from searchengines';
 
   let keywords;
-  let companies;
+  let urls;
   let searchEngines;
 
   db.query(searchEngineQuery, (error, result) => {
