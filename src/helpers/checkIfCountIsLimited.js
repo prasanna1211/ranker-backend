57c71e669353d38e8a2c17a7107d9a61ae582ca3
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-15 13:49:11 +0530

    addd function to check count

--- src/helpers/checkIfCountIsLimited.js
+++ src/helpers/checkIfCountIsLimited.js
@@ -3,13 +3,17 @@ const forEach = require('lodash/forEach');
 module.exports = {
   check: (batch, maxCount) => {
     let result = true;
+    let maxValue = 0;
     forEach(batch, (value) => {
       let count = 0;
       forEach(batch, (inner) => {
-        count += inner == value;
+        count += (inner === value);
       });
       result &= (count <= maxCount);
+      maxValue = (count >= maxValue) ? count : maxValue;
+
     });
+    // console.log('maxValue', maxValue, result);
     return result;
   }
 };
