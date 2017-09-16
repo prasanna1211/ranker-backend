ff86eb827fd2dcc99d931de6117e9611752ee1cf
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-16 18:04:15 +0530

    added task cancellation

--- src/helpers/getGoogleResult.js
+++ src/helpers/getGoogleResult.js
@@ -29,58 +29,64 @@ async function scrapeGoogleResult(keyword, domain, numberOfPages) {
     pageWaitDurationRangeStart: start,
     pageWaitDurationRangeEnd: end,
   } = appConstants;
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+    page.setUserAgent(randomUserAgents[getRandomSeconds(0, 8)]);
+    await page.goto('https://www.google.com/ncr', {
+      timeout: 60000,
+    });
 
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
-  const page = await browser.newPage();
-  page.setUserAgent(randomUserAgents[getRandomSeconds(0, 8)]);
-  await page.goto('https://www.google.com/ncr');
-  await page.goto(domain);
+    await page.goto(domain);
 
-  page.focus('input[title="Search"]');
+    page.focus('input[title="Search"]');
 
-  await page.type(keyword, { delay: getRandomSeconds(100, 500) });
+    await page.type(keyword, { delay: getRandomSeconds(100, 500) });
 
-  const submit = await page.$('input[value="Google Search"]');
-  await submit.click();
+    const submit = await page.$('input[value="Google Search"]');
+    await submit.click();
 
-  await page.waitForNavigation();
-
-  // const result
-  const pageOneText = await page.plainText();
-  if (isNull(pageOneText)) {
-    return {
-      error: true,
-      errorMessage: 'Failed to fetch page 1'
-    };
-  }
-  let result = Array.from(getUrls(pageOneText));
-
-  let index = 1;
-  for (var i = index + 1; i <= numberOfPages ; i++) {
-    const selector = `a[aria-label="Page ${i}"]`;
-    const nextPageNavigateButton = await page.$(selector);
-
-    await page.waitFor(getRandomSeconds(start, end));
-    await nextPageNavigateButton.click();
     await page.waitForNavigation();
 
-    const currentPageText = await page.plainText();
-    if (isNull(currentPageText)) {
+    // const result
+    const pageOneText = await page.plainText();
+    if (isNull(pageOneText)) {
       return {
         error: true,
-        errorMessage: `Failed to fetch page ${i}`
+        errorMessage: 'Failed to fetch page 1'
       };
     }
-    result = concat(result, Array.from(getUrls(currentPageText)));
-  }
+    let result = Array.from(getUrls(pageOneText));
 
-  browser.close();
-  return {
-    success: true,
-    result,
-  };
+    let index = 1;
+    for (var i = index + 1; i <= numberOfPages ; i++) {
+      const selector = `a[aria-label="Page ${i}"]`;
+      const nextPageNavigateButton = await page.$(selector);
+
+      await page.waitFor(getRandomSeconds(start, end));
+      await nextPageNavigateButton.click();
+      await page.waitForNavigation();
+
+      const currentPageText = await page.plainText();
+      if (isNull(currentPageText)) {
+        return {
+          error: true,
+          errorMessage: `Failed to fetch page ${i}`
+        };
+      }
+      result = concat(result, Array.from(getUrls(currentPageText)));
+    }
+    await page.close();
+    browser.close();
+    return {
+      success: true,
+      result,
+    };
+  } catch (e) {
+    throw new Error(`Error occured ${e}`);
+  }
 }
 
 module.exports = {
