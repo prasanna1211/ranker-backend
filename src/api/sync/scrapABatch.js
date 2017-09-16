055bc09689329b38a81e223e25740df92218dcc5
prasanna1211 <prasanna.krystal@gmail.com>
2017-09-16 18:03:34 +0530

    commented certain part

--- src/api/sync/scrapABatch.js
+++ src/api/sync/scrapABatch.js
@@ -3,43 +3,65 @@ let cron = require('node-cron');
 
 const map = require('lodash/map');
 const random = require('lodash/random');
+const filter = require('lodash/filter');
+const findIndex = require('lodash/findIndex');
+const moment = require('moment');
 
 const googleResult = require('../../helpers/getGoogleResult.js');
 
-const scrapABatch = (batch, currentBatchNumber) => {
-  console.log('--------------------', batch);
+const rankWriteDbQuery = (rank, logDate, companyId, keywordId, searchEngineId) => `INSERT INTO ranks (rank, logDate, companyId, keywordId, searchEngineId) VALUES (${rank}, "${logDate}", ${companyId}, ${keywordId}, ${searchEngineId})`;
+
+const scrapABatch = (db, taskList, batch, currentBatchNumber, urls) => {
+  console.log('batch', batch);
   if (!batch.length) {
     return [];
   }
+
+  const currentDate = moment().startOf('day').format('YYYY-MM-DD');
+
   for (var k = 0; k < batch.length; k++) {
 
     const { keyword, keywordId, isImportant, domain, domainId } = batch[k];
-    console.log('keyword', keyword);
-    // const keywordScrapStartTime = `${5 + (k * 10) + parseInt(random(0, 2))}`;
-    // const cronString = `0 ${keywordScrapStartTime} ${currentBatchNumber} * * *`;
-    const cronString = `30 ${(25+(k*5))%60} 10 * * *`;
-    console.log(cronString);
-    cron.schedule(cronString, () => {
-      try {
-        const results = googleResult
-        .scrapeGoogleResult(keyword, 'https://www.google.co.in', 3)
-        .then((scrap) => {
-          if (scrap.success) {
-            // IF successs write it to db.
-
-            console.log('result ', scrap.result);
-          } else if (scrap.error) {
-            console.log('error', scrap.error);
-            // Some internal error write to status db
-          }
-        });
-      } catch (e) {
-        // Some other error for this particular keyword.
-        // still write on error db
-      }
-
-
-    })
+    const keywordScrapStartTime = `${13 + (k * 10) + parseInt(random(0, 2))}`;
+    const cronString = `0 ${keywordScrapStartTime} ${currentBatchNumber} * * *`;
+    console.log('scheduled for this hour ', cronString, keyword, domain);
+    // const cronString = `10 ${(0+(k*6))%60} 16 * * *`;
+    // taskList[currentBatchNumber][k] = cron.schedule(cronString, () => {
+    //   try {
+    //     const results = googleResult
+    //     .scrapeGoogleResult(keyword, 'https://www.google.co.in', 3)
+    //     .then((scrap) => {
+    //       if (scrap.success) {
+    //         // IF successs write it to db.
+    //         const currentUrls = filter(urls, url => url.domain === domain);
+    //         const resultWithRank = map(currentUrls, (urlData) => {
+    //           console.log('result: ', scrap.result, urlData.url, findIndex(scrap.result, urlData.url))
+    //           return {
+    //             ...urlData,
+    //             rank: findIndex(scrap.result, eachUrl => eachUrl === urlData.url)
+    //           }
+    //         });
+    //         console.log(resultWithRank);
+    //         map(resultWithRank, (result) => {
+    //           db.query(rankWriteDbQuery(rank, currentDate, result.companyId, keywordId, 1), (error, result) => {
+    //             if (error) {
+    //               console.log(error);
+    //             }
+    //             console.log('1 record successfully written')
+    //           });
+    //         });
+    //       } else if (scrap.error) {
+    //         console.log('error', scrap.error);
+    //         // Some internal error write to status db
+    //       }
+    //     console.log('finished');
+    //     });
+    //   } catch (e) {
+    //     // Some other error for this particular keyword.
+    //     // still write on error db
+    //   }
+    //
+    // });
   }
 }
 
